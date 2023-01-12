9c787b2b59802d9bc238644244c7d797a54fe099
Dionei Silva <dionei.silva@gmail.com>
2023-01-12 17:46:31 -0300

    Segundo commit

--- Cadastro/cadastro.js
+++ Cadastro/cadastro.js
@@ -2,12 +2,30 @@ let nomeUsuario = document.getElementById("cadastro-usuario");
 let senhaUsuario = document.getElementById("cadastro-senha");
 let repeteSenhaUsuario = document.getElementById("repete-senha");
 function cadastrar() {
+    if (senhaUsuario.value !== repeteSenhaUsuario.value) {
+        alert("Senhas nao conferem, Tente Novamente");
+        return;
+    }
+
     let listaUsuarios = JSON.parse(
         localStorage.getItem("listaUsuarios") || "[]"
     );
+
     listaUsuarios.push({
         nome: nomeUsuario.value,
         senha: senhaUsuario.value,
     });
+
     localStorage.setItem("listaUsuarios", JSON.stringify(listaUsuarios));
+
+    /*     buscaEmail = JSON.parse(localStorage.getItem("listaUsuarios"));
+    let encontraEmail = buscaEmail.some(
+        (valor) => valor.nome == nomeUsuario.value
+    );
+    console.log("encontraEmail");
+    if (encontraEmail)
+        alert(
+            "Ja existe um usuario cadastrado com esse email. Tente novamente!!"
+        );
+    window.location.href = "./Login/login.html"; */
 }
--- Login/login.js
+++ Login/login.js
@@ -1,12 +1,16 @@
-let inputRecado = document.querySelector("#input-recados");
-let inputCadastrar = document.querySelector("#input-cadastrar");
-// fazer um para cada
-inputRecado.onclick = (e) => {
-    e.preventDefault();
-    window.location.href = "../Recados/recados.html";
-};
-/* function encontrarContato() {
-    let encontrarContato = listaUsuarios.findIndex(
+let usuario = document.getElementById("email");
+let senha = document.getElementById("senha");
+function encontrarContato() {
+    /*     let encontrarContato = encontraUsuario.some(
         (valor) => valor.nome === nomeUsuario
+    ); */
+    let encontraUsuario = [];
+    encontraUsuario = JSON.parse(localStorage.getItem("listaUsuarios"));
+    /* console.log(encontraUsuario[0].nome); */
+    let encontrarNome = encontraUsuario.some(
+        (valor) => valor.nome == usuario.value && valor.senha == senha.value
     );
-} */
+    /* console.log(encontrarNome); */
+    if (encontrarNome) window.location.href = "../Recados/recados.html";
+    else alert("Email e senha Invalidos. Tente novamente!!");
+}
