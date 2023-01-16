fc83c1c7aa61e930a1c328629d8da9bed4e6ec34
Dionei Silva <dionei.silva@gmail.com>
2023-01-16 14:13:43 -0300

    Validação preenchimento de todos os campos

--- Cadastro/cadastro.js
+++ Cadastro/cadastro.js
@@ -2,31 +2,38 @@ let nomeUsuario = document.getElementById("cadastro-usuario");
 let senhaUsuario = document.getElementById("cadastro-senha");
 let repeteSenhaUsuario = document.getElementById("repete-senha");
 function cadastrar() {
-    if (senhaUsuario.value !== repeteSenhaUsuario.value) {
-        alert("Senhas nao conferem, Tente Novamente");
+    if (
+        nomeUsuario.value !== "" &&
+        senhaUsuario.value !== "" &&
+        repeteSenhaUsuario.value !== ""
+    ) {
+        if (senhaUsuario.value !== repeteSenhaUsuario.value) {
+            alert("Senhas nao conferem, Tente Novamente");
+            return;
+        }
+
+        let listaUsuarios = JSON.parse(
+            localStorage.getItem("listaUsuarios") || "[]"
+        );
+        let cadastrado = listaUsuarios.some(
+            (valor) => valor.nome == nomeUsuario.value
+        );
+        if (cadastrado) {
+            alert("Usuario ja cadastrado, Tente outro Email !!");
+            return;
+        } else {
+            listaUsuarios.push({
+                nome: nomeUsuario.value,
+                senha: senhaUsuario.value,
+                recados: [],
+            });
+        }
+
+        localStorage.setItem("listaUsuarios", JSON.stringify(listaUsuarios));
+
+        window.location.href = "../Login/login.html";
+    } else {
+        alert("Preencha todos os campos!");
         return;
     }
-
-    let listaUsuarios = JSON.parse(
-        localStorage.getItem("listaUsuarios") || "[]"
-    );
-
-    listaUsuarios.push({
-        nome: nomeUsuario.value,
-        senha: senhaUsuario.value,
-        recados: [],
-    });
-
-    localStorage.setItem("listaUsuarios", JSON.stringify(listaUsuarios));
-
-    /*     buscaEmail = JSON.parse(localStorage.getItem("listaUsuarios"));
-    let encontraEmail = buscaEmail.some(
-        (valor) => valor.nome == nomeUsuario.value
-    );
-    console.log("encontraEmail");
-    if (encontraEmail)
-        alert(
-            "Ja existe um usuario cadastrado com esse email. Tente novamente!!"
-        );
-    window.location.href = "./Login/login.html"; */
 }
