c9808049af0852f4608f43fc5bff1a6ee93a8337
Dionei Silva <dionei.silva@gmail.com>
2023-01-16 14:30:09 -0300

    validação de input vazio

--- Login/login.js
+++ Login/login.js
@@ -1,16 +1,21 @@
 let usuario = document.getElementById("email");
 let senha = document.getElementById("senha");
 function encontrarContato() {
-    /*     let encontrarContato = encontraUsuario.some(
-        (valor) => valor.nome === nomeUsuario
-    ); */
     let encontraUsuario = [];
+
     encontraUsuario = JSON.parse(localStorage.getItem("listaUsuarios"));
-    /* console.log(encontraUsuario[0].nome); */
+
     let encontrarNome = encontraUsuario.some(
         (valor) => valor.nome == usuario.value && valor.senha == senha.value
     );
-    /* console.log(encontrarNome); */
+
     if (encontrarNome) window.location.href = "../Recados/recados.html";
     else alert("Email e senha Invalidos. Tente novamente!!");
+
+    let usuarioLogado = {
+        email: usuario.value,
+        recados: [],
+    };
+
+    localStorage.setItem("usuarioLogado", JSON.stringify(usuarioLogado));
 }
