26c580c1d5e613fbcb2d2c77cf4ba0ecdab244da
Dionei Silva <dionei.silva@gmail.com>
2023-01-21 16:49:49 -0300

    btn-editar-ok

--- Recados/recados.js
+++ Recados/recados.js
@@ -1,7 +1,7 @@
 let descricao = document.querySelector("#descricaoUsuario");
 let detalhamento = document.querySelector("#detalhamentoUsuario");
 let usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"));
-let btnEditar = document.querySelector("#btnEditar");
+let salvar = document.querySelector("#salvar");
 if (usuarioLogado === null) {
     alert("Acesso a pagina, somente com login!!");
     window.location.href = "../Login/login.html";
@@ -38,19 +38,11 @@ function mostrarRecados() {
             <h4>Detalhamento: ${usuario.detalhamento}</h4>
             </div>
             <div class="div-btn">
-                <button onclick="editarRecado(${i})" class="button-verde">Editar</button>
+                <button id = 'btnInserir' onclick="editarRecado(${i})" class="button-verde">Editar</button>
                 <button onclick="apagarRecado(${i})" class="button-vermelho">Apagar</button>
             </div>
             </div>
         `;
-
-        // const divDescricao = document.createElement("div");
-        // divDescricao.innerHTML = `${usuario.descricao.value}`;
-        // recado.appendChild(divDescricao);
-
-        // const divDetalhamento = document.createElement("div");
-        // divDescricao.innerHTML = `${usuario.detalhamento.value}`;
-        // recado.appendChild(divDetalhamento);
 
         olHTML.appendChild(recado);
         salvarRecadosJSON();
@@ -83,61 +75,28 @@ function apagarRecado(i) {
 }
 
 function editarRecado(i) {
-    /* btnEditar.setAttribute("style", "display:none"); */
     let editarRecado = usuarioLogado.recados[i];
+
+    salvar.setAttribute("onclick", `salvarEdicao(${i})`);
+    salvar.innerHTML = "Editar";
 
     descricao.value = editarRecado.descricao;
     detalhamento.value = editarRecado.detalhamento;
 
-    if (
-        descricao.value !== editarRecado.descricao ||
-        detalhamento.value !== editarRecado.detalhamento
-    ) {
-        usuarioLogado.recados[i].push({
-            descricao: descricao.value,
-            detalhamento: detalhamento.value,
-        });
-    }
-
     console.log(usuarioLogado.recados[i]);
 }
 
-//descricao.focus();
-
-/* btnSalvarEditado.addEventListener("click", () => {
-    if (descricao.value !== "" && detalhamento.value !== "") {
-        userLogado.post[index] = {
-            descricao: descricao.value,
-            detalhamento: detalhamento.value,
-        };
-        updateDB();
-        descricao.value = "";
-        detalhamento.value = "";
-        btnInserir.setAttribute("style", "display:block");
-        btnSalvarEditado.setAttribute("style", "display:none");
+function salvarEdicao(i) {
+    if (descricao.value !== "" || detalhamento.value !== "") {
+        usuarioLogado.recados[i].descricao = descricao.value;
+        usuarioLogado.recados[i].detalhamento = detalhamento.value;
+    } else {
+        return alert("Preencha os campos necessarios!!");
     }
-}); */
+    salvar.setAttribute("onclick", `listarRecados()`);
+    salvar.innerHTML = "Salvar";
 
-// function editar(i) {
-//   btnInserir.setAttribute("style", "display:none");
-//   btnSalvarEditado.setAttribute("style", "display:block");
-//   tarefa.value = userLogado.post[i].tarefa;
-//   descricao.value = userLogado.post[i].descricao;
-
-//   tarefa.focus();
-//   index = i;
-// }
-
-// btnSalvarEditado.addEventListener("click", () => {
-//   if (tarefa.value !== "" && descricao.value !== "") {
-//     userLogado.post[index] = {
-//       tarefa: tarefa.value,
-//       descricao: descricao.value,
-//     };
-//     updateDB();
-//     tarefa.value = "";
-//     descricao.value = "";
-//     btnInserir.setAttribute("style", "display:block");
-//     btnSalvarEditado.setAttribute("style", "display:none");
-//   }
-// });
+    descricao.value = "";
+    detalhamento.value = "";
+    mostrarRecados();
+}
